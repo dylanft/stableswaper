d5e1c56f4d6ed3f1541a8f16d77813eb49e88a9b
dylanft <dylan@wildwestdigital.io>
2022-08-15 14:45:19 -0400

    added links in toolbar. home and swap content displays dynamically

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stacks-angular';
+  content: string = "home";
+
+  setContent(c: string) {
+    this.content = c;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ConnectWalletComponent } from './connect-wallet/connect-wallet.component';
@@ -12,7 +13,10 @@ import { ContractVoteComponent } from './contract-vote/contract-vote.component';
     ContractVoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'vote', component: ContractVoteComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
