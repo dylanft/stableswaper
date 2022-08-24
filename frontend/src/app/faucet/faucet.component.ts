56ae32784112533cbef748e440b014bed6bf82f1
dylanft <dylan@wildwestdigital.io>
2022-08-24 17:21:21 -0400

    connected the mint function to devnet

--- frontend/src/app/faucet/faucet.component.ts
+++ frontend/src/app/faucet/faucet.component.ts
@@ -13,6 +13,7 @@ import {
   stringUtf8CV,
   uintCV,
 } from '@stacks/transactions';
+import { principalCV } from '@stacks/transactions/dist/clarity/types/principalCV';
 import { userSession } from 'src/stacksUserSession';
 
 @Component({
@@ -27,19 +28,20 @@ export class FaucetComponent implements OnInit {
   ngOnInit(): void {
   }
   tokenList: any[] = ['USDA', 'xUSD'];
-  tokenA: string = '';
-  tokenB: string = '';
-  tokenA_amt: string = "0";
-  tokenB_amt: string = "0";
+  tokenChoice: string = '';
+  tokenChoiceContract: any;
+  tokenChoiceContractName: string = '';
+  mintAmount: number = 0;
+  deployerAddress: string = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM';
 
   network: any = new StacksMocknet();
   usdaContract: ContractPrincipalCV = contractPrincipalCVFromAddress(
-      createAddress('ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'),
+      createAddress(this.deployerAddress),
       createLPString('usda-token')
     );
 
   xusdContract: ContractPrincipalCV = contractPrincipalCVFromAddress(
-    createAddress('ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'),
+    createAddress(this.deployerAddress),
     createLPString('xusd-token')
   );
 
@@ -87,17 +89,71 @@ export class FaucetComponent implements OnInit {
   selectToken(event: any, tokenAorB: string) {
     // console.log("event: ", event)
     // console.log("event.value: ", event.value)
-    if (tokenAorB == "A") {
-      this.tokenA = event.value;
-      console.log("tokenA: ", this.tokenA);
+    // if (tokenAorB == "A") {
+    //   this.tokenA = event.value;
+    //   console.log("tokenA: ", this.tokenA);
+    // }
+    // else if (tokenAorB == "B") {
+    //   this.tokenB = event.value;
+    //   console.log("tokenB: ", this.tokenB);
+    // }
+    this.tokenChoice = event.value;
+    if (this.tokenChoice == 'USDA') {
+      this.tokenChoiceContract = this.usdaContract;
+      this.tokenChoiceContractName = 'usda-token';
     }
-    else if (tokenAorB == "B") {
-      this.tokenB = event.value;
-      console.log("tokenB: ", this.tokenB);
+    else {
+      this.tokenChoiceContract = this.xusdContract;
+      this.tokenChoiceContractName = 'xusd-token';
     }
+    console.log("tokenChoice: ", this.tokenChoice);
+    console.log("tokenChoiceContract", this.tokenChoiceContract);
+    console.log("tokenChoiceContract", this.tokenChoiceContract.toString());
+
   }
 
-  printTokenAmount(val: string) {
+  mintTokens() {
+    console.log("minting ", this.mintAmount, " of ", this.tokenChoice);
+    console.log(userSession.loadUserData().profile.stxAddress.testnet);
+    var txSenderAddress: string;
+    if (this.network.isMainnet()) {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.mainnet;
+      console.log(txSenderAddress)
+    }
+    else {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.testnet;
+    }
+
+    openContractCall({
+      network: this.network,
+      anchorMode: AnchorMode.Any,
+      contractAddress: this.deployerAddress,
+      // contractName: this.tokenChoiceContract.contractName.content,
+      contractName: this.tokenChoiceContractName,
+      functionName: 'mint',
+      functionArgs: [uintCV(this.mintAmount), principalCV(txSenderAddress)],
+      postConditionMode: PostConditionMode.Deny,
+      postConditions: [],
+      onFinish: (data) => {
+        console.log('onFinish:', data);
+        window
+          ?.open(
+            `http://localhost:8000/txid/${data.txId}?chain=testnet`,
+            '_blank'
+          )
+          ?.focus();
+      },
+      onCancel: () => {
+        console.log('onCancel:', 'Transaction was canceled');
+      },
+    });
+
+
+  }
+
+  setMintAmount(val: string) {
     console.log(val)
+    this.mintAmount = parseInt(val);
+
   }
 }
