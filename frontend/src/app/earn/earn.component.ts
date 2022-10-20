e023ae7a150b7523c21cacb8f63a86cf42bc1c49
dylanft <dylan@bitflow.finance>
2022-10-20 15:20:20 -0400

    stack box on earn page shows balance based on token selected

--- frontend/src/app/earn/earn.component.ts
+++ frontend/src/app/earn/earn.component.ts
@@ -56,6 +56,11 @@ export class EarnComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.getUsersTokenBalance('USDA', 1e6).then((x) => this.usdaBalance = x);
+    this.getUsersTokenBalance('xUSD', 1e6).then((x) => this.xusdBalance = x);
+    this.getUsersTokenBalance('xBTC', 1).then((x) => this.xbtcBalance = x);
+    this.getUsersTokenBalance('usd-lp', 1e6).then((x) => this.usdlpBalance = x);
+
   }
   public userSession = userSession;
   deployerAddress: string = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW';
@@ -67,7 +72,10 @@ export class EarnComponent implements OnInit {
   cycleClaimNumber: number = 1;
   currentCycle: number | null = null;
   network: any = new StacksTestnet();
-
+  usdaBalance: string | null = null;
+  xusdBalance: string | null = null;
+  xbtcBalance: string | null = null;
+  usdlpBalance: string | null = null;
 
   tokenA: string = '';
   tokenB: string = '';
@@ -781,6 +789,62 @@ export class EarnComponent implements OnInit {
     var toal_xbtc_escrowed = output['amount'].value
     console.log("toal_xbtc_escrowed: ",toal_xbtc_escrowed)
     return toal_xbtc_escrowed
+  }
+
+  async getUsersTokenBalance(token: string, decimalFactor: number) {
+    var txSenderAddress: string;
+
+    if (this.network.isMainnet()) {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.mainnet;
+      console.log(txSenderAddress)
+    }
+    else {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.testnet;
+    }
+
+    if (token == 'USDA') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'usda-token'
+    } 
+    else if (token == 'xUSD') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'xusd-token'
+    } 
+    else if (token == 'xBTC') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'xbtc-token'
+    } 
+    else if (token == 'usd-lp') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'usd-lp'
+    } 
+    else {
+      var contractAddress = ''
+      var contractName = ''
+      console.log(token + " balance lookup not included with app code")
+    } 
+
+    var options = {
+      network: this.network,
+      contractAddress: contractAddress,
+      contractName: contractName,
+      functionName: 'get-balance',
+      functionArgs: [principalCV(txSenderAddress)],
+      senderAddress: txSenderAddress
+
+    }
+    const result = await callReadOnlyFunction(options);
+    var output = cvToValue(result);
+    return this.numberWithCommas(output.value, decimalFactor);
+
+  }
+
+  numberWithCommas(n: number, uintDecimalFactor: number) {
+    var n_str = (n / uintDecimalFactor).toString();
+    var x = Number(parseFloat(n_str)).toFixed(2);
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
   }
 
 }
