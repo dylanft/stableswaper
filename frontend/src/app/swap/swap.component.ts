55e26bef2c91adfe41e95e1872d3c389c5650710
dylanft <dylan@bitflow.finance>
2022-10-20 13:42:35 -0400

    swap box shows balance based on token selected

--- frontend/src/app/swap/swap.component.ts
+++ frontend/src/app/swap/swap.component.ts
@@ -3,6 +3,7 @@ import { openContractCall } from '@stacks/connect';
 import { StacksMocknet, StacksTestnet } from '@stacks/network';
 import {
   AnchorMode,
+  callReadOnlyFunction,
   ContractPrincipal,
   contractPrincipalCV,
   ContractPrincipalCV,
@@ -19,6 +20,7 @@ import {
   stringAsciiCV,
   stringUtf8CV,
   uintCV,
+  cvToValue
 } from '@stacks/transactions';
 import { principalCV } from '@stacks/transactions/dist/clarity/types/principalCV';
 import { userSession } from 'src/stacksUserSession';
@@ -41,6 +43,10 @@ export class SwapComponent implements OnInit {
     else {
       this.txSenderAddress = userSession.loadUserData().profile.stxAddress.testnet;
     }
+    this.getUsersTokenBalance('USDA', 1e6).then((x) => this.usdaBalance = x);
+    this.getUsersTokenBalance('xUSD', 1e6).then((x) => this.xusdBalance = x);
+    this.getUsersTokenBalance('xBTC', 1).then((x) => this.xbtcBalance = x);
+    this.getUsersTokenBalance('usd-lp', 1e6).then((x) => this.usdlpBalance = x);
   }
   tokenList: any[] = ['USDA', 'xUSD'];
   deployerAddress: string = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW';
@@ -64,7 +70,10 @@ export class SwapComponent implements OnInit {
 
 
   public userSession = userSession;
-
+  usdaBalance: string | null = null;
+  xusdBalance: string | null = null;
+  xbtcBalance: string | null = null;
+  usdlpBalance: string | null = null;
 
   swapXforY(tokenX: string, tokenY: string, x: number, y: number) {
     x = x*1e6;
@@ -160,4 +169,60 @@ export class SwapComponent implements OnInit {
       console.log("tokenB: ", this.tokenB_amt);
     }
   }
+  async getUsersTokenBalance(token: string, decimalFactor: number) {
+    var txSenderAddress: string;
+
+    if (this.network.isMainnet()) {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.mainnet;
+      console.log(txSenderAddress)
+    }
+    else {
+      txSenderAddress = userSession.loadUserData().profile.stxAddress.testnet;
+    }
+
+    if (token == 'USDA') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'usda-token'
+    } 
+    else if (token == 'xUSD') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'xusd-token'
+    } 
+    else if (token == 'xBTC') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'xbtc-token'
+    } 
+    else if (token == 'usd-lp') {
+      var contractAddress = 'ST38GBVK5HEJ0MBH4CRJ9HQEW86HX0H9AP3EJP4TW'
+      var contractName = 'usd-lp'
+    } 
+    else {
+      var contractAddress = ''
+      var contractName = ''
+      console.log(token + " balance lookup not included with app code")
+    } 
+
+    var options = {
+      network: this.network,
+      contractAddress: contractAddress,
+      contractName: contractName,
+      functionName: 'get-balance',
+      functionArgs: [principalCV(txSenderAddress)],
+      senderAddress: txSenderAddress
+
+    }
+    const result = await callReadOnlyFunction(options);
+    var output = cvToValue(result);
+    return this.numberWithCommas(output.value, decimalFactor);
+
+  }
+
+  numberWithCommas(n: number, uintDecimalFactor: number) {
+    var n_str = (n / uintDecimalFactor).toString();
+    var x = Number(parseFloat(n_str)).toFixed(2);
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+  }
+
 }
